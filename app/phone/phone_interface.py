89fb1a1cb6ef2ef8fb2c83b73637b4283b326ff9
danvf <dvf@cin.ufpe.br>
2020-09-11 14:26:11 -0300

    basic project running, still needs input and output treatment

--- app/phone/phone_interface.py
+++ app/phone/phone_interface.py
@@ -1,24 +1,31 @@
-from .phone_state import InitialScreen
-from ..util import constants
+from phone.phone_states import InitialScreen
+from util import constants
 
 
 class PhoneInterface:
     def __init__(self):
-        self._state = InitialScreen
+        self._state = InitialScreen()
+        self._state.context = self
         print(constants.INITIAL_SCREEN)
 
     def switch_state(self, new_state):
         self._state = new_state
         self._state.context = self
 
-    def press_button_call(self): self._state.button_call()
+    def press_button_call(self):
+        self._state.button_call()
 
-    def press_button_dismiss(self): self._state.button_dismiss()
+    def press_button_dismiss(self):
+        self._state.button_dismiss()
 
-    def flag_avatar_displayed(self): self._state.avatar_displayed()
+    def flag_avatar_displayed(self):
+        self._state.avatar_displayed()
 
-    def flag_popup_no_network(self): self._state.popup_no_network()
+    def flag_popup_no_network(self):
+        self._state.popup_no_network()
 
-    def flag_popup_call_dismissed(self): self._state.popup_call_dismissed()
+    def flag_popup_call_dismissed(self):
+        self._state.popup_call_dismissed()
 
-    def flag_popup_ending_call(self): self._state.popup_ending_call()
+    def flag_popup_ending_call(self):
+        self._state.popup_ending_call()
--- app/phone/phone_states.py
+++ app/phone/phone_states.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from ..util import constants
+from util import constants
 
 
 class PhoneActions(ABC):
@@ -34,7 +34,7 @@ class PhoneActions(ABC):
 class InitialScreen(PhoneActions):
     def button_call(self):
         print(constants.INTERMEDIATE_SCREEN)
-        self.context.switch_state(IntermediateScreen)
+        self.context.switch_state(IntermediateScreen())
 
     def button_dismiss(self):
         print(constants.INPUT_ERROR)
@@ -58,19 +58,19 @@ class IntermediateScreen(PhoneActions):
 
     def button_dismiss(self):
         print(constants.INITIAL_SCREEN)
-        self.context.switch_state(InitialScreen)
+        self.context.switch_state(InitialScreen())
 
     def avatar_displayed(self):
         print(constants.CALL_SCREEN)
-        self.context.switch_state(CallScreen)
+        self.context.switch_state(CallScreen())
 
     def popup_no_network(self):
         print(constants.INITIAL_SCREEN)
-        self.context.switch_state(InitialScreen)
+        self.context.switch_state(InitialScreen())
 
     def popup_call_dismissed(self):
         print(constants.INITIAL_SCREEN)
-        self.context.switch_state(InitialScreen)
+        self.context.switch_state(InitialScreen())
 
     def popup_ending_call(self):
         print(constants.INPUT_ERROR)
@@ -82,19 +82,19 @@ class CallScreen(PhoneActions):
 
     def button_dismiss(self):
         print(constants.INITIAL_SCREEN)
-        self.context.switch_state(InitialScreen)
+        self.context.switch_state(InitialScreen())
 
     def avatar_displayed(self):
         print(constants.INPUT_ERROR)
 
     def popup_no_network(self):
         print(constants.INITIAL_SCREEN)
-        self.context.switch_state(InitialScreen)
+        self.context.switch_state(InitialScreen())
 
     def popup_call_dismissed(self):
         print(constants.INITIAL_SCREEN)
-        self.context.switch_state(InitialScreen)
+        self.context.switch_state(InitialScreen())
 
     def popup_ending_call(self):
         print(constants.INITIAL_SCREEN)
-        self.context.switch_state(InitialScreen)
+        self.context.switch_state(InitialScreen())
